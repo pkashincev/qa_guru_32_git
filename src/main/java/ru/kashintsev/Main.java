ab4dc1a6b263fec6d38f21b8c0cadb8e201904bb
Pavel Kashincev <pavelkashincev@gmail.com>
2024-11-25 23:24:45 +0300

    Update Main.java

--- src/main/java/ru/kashintsev/Main.java
+++ src/main/java/ru/kashintsev/Main.java
@@ -1,7 +1,10 @@
 package ru.kashintsev;
 
 public class Main {
+
+    private static String msg = "Hello, qa.guru!!!!!!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello, qa.guru!!!!!!");
+        System.out.println(msg);
     }
 }
